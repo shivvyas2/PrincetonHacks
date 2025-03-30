9b88a6998295ff2c6a75601f2e25291606217111
Vighanesh <vgaund@nyit.edu>
2025-03-29 21:38:53 -0400

    created a backend

--- financeApp/services/api.ts
+++ financeApp/services/api.ts
@@ -8,8 +8,11 @@ const localIp = manifestProp ? manifestProp.split(':')[0] : '10.29.251.136';
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const API_URL = `http://${localIp}:5001/api`;
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 // Use local IP in development, production URL in production
@@ -17,6 +20,9 @@ const API_URL = 'https://finance-app-backend-d3ay.onrender.com/api';
 
 console.log('Using API URL:', API_URL); // Debug log
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
